7b0f10c784eb3f16409e977b349e0130656a3acc
Alexey <loskuta42@yandex.ru>
2021-03-15 21:24:40 +0300

    done test_urls

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from posts.models import Group
+from posts.models import Group, Post
 
 User = get_user_model()
 
@@ -12,24 +12,71 @@ class PostURLTests(TestCase):
         # Устанавливаем данные для тестирования
         # Создаём экземпляр клиента. Он неавторизован.
         cls.guest_client = Client()
-        cls.user = User.objects.create_user(username='testuser')
-        cls.authorized_client = Client()
-        cls.authorized_client.force_login(cls.user)
+        cls.author = User.objects.create_user(username='test_author')
+        cls.authorized_author_client = Client()
+        cls.authorized_author_client.force_login(cls.author)
+        cls.not_author = User.objects.create_user(username='test_not_author')
+        cls.authorized_not_author_client = Client()
+        cls.authorized_not_author_client.force_login(cls.not_author)
         cls.group = Group.objects.create(
             title='test_group',
             slug='test-slug',
             description='test_description'
         )
+        cls.post = Post.objects.create(
+            id=1,
+            text='test_post',
+            group=cls.group,
+            author=cls.author
+        )
         cls.templates_url_names = {
-            'index.html': '/',
-            'group.html': '/group/test-slug/',
-            'new.html': '/new/'
+            '/': 'index.html',
+            '/group/test-slug/': 'group.html',
+            '/new/': 'new.html',
+            '/test_author/1/edit/': 'new.html',
+            '/test_author/': 'profile.html',
+            '/test_author/1/': 'post.html'
         }
 
-    def test_homepage(self):
+    def test_index(self):
         """Страница / доступна любому пользователю."""
         response = PostURLTests.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_profile(self):
+        """Страница /<username>/ доступна любому пользователю."""
+        response = PostURLTests.guest_client.get('/test_author/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_post(self):
+        """Страница /<username>/<post_id>/ доступна любому пользователю."""
+        response = PostURLTests.guest_client.get('/test_author/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_post_edit_not_auth(self):
+        """Страница /<username>/<post_id>/edit/ перенаправит анонимного
+        пользователя на страницу логина.
+        """
+        response = PostURLTests.guest_client.get('/test_author/1/edit/', follow=True)
+        self.assertRedirects(
+            response, '/auth/login/?next=/test_author/1/edit/')
+
+    def test_profile_post_edit_auth_not_author(self):
+        """Страница /<username>/<post_id>/edit/ перенаправит
+         авторизированного пользователя(не автора поста) на страницу поста.
+        """
+        response = PostURLTests.authorized_not_author_client.get('/test_author/1/edit/', follow=True)
+        self.assertRedirects(
+            response, '/test_author/1/')
+
+    def test_profile_post_edit_auth_author(self):
+        """Страница /<username>/<post_id>/edit/ доступна
+         автору поста.
+        """
+        response = PostURLTests.authorized_author_client.get('/test_author/1/edit/')
+        self.assertEqual(response.status_code, 200)
+
+
 
     def test_group_slug(self):
         """Страница /group/test-slug/ доступна любому пользователю."""
@@ -38,14 +85,14 @@ class PostURLTests(TestCase):
 
     def test_new(self):
         """Страница /new/ доступна авторизованному пользователю."""
-        response = PostURLTests.authorized_client.get('/new/')
+        response = PostURLTests.authorized_author_client.get('/new/')
         self.assertEqual(response.status_code, 200)
 
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
-        for template, url in PostURLTests.templates_url_names.items():
+        for url, template in PostURLTests.templates_url_names.items():
             with self.subTest(url=url):
-                response = self.authorized_client.get(url)
+                response = PostURLTests.authorized_author_client.get(url)
                 self.assertTemplateUsed(response, template)
 
 
